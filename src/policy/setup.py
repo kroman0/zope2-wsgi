db7a45c87553435e2db1b51b3024e155bacde36e
Hanno Schlichting <hanno@jarn.com>
2011-01-07 21:45:16 +0100

    Wire in a distribution / package

---
+++ src/policy/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(name='policy',
+    version='1.0dev',
+    url='http://localhost',
+    license='Private',
+    description='Policy',
+    author='Company',
+    author_email='info@company.com',
+    long_description='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'PasteScript',
+        'repoze.retry',
+        'repoze.tm2',
+        'repoze.who',
+        'setuptools',
+        'Zope2',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+    },
+)
